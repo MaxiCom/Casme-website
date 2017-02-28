f61148fe3dfd9989d6dc3b57ad8296844dd0a3bd
maxime morlet <maximemorlet@hotmail.fr>
2017-02-28 15:22:39 +0100

    ok

--- App/public/index.php
+++ App/public/index.php
@@ -17,7 +17,7 @@
 
 		<style type="text/css">
 			header {
-				background: url("/resources/images/header/background<?php echo $random_backgroud_number; ?>.jpg") no-repeat;
+				background: url("/resources/images/header/background-plomberie-<?php echo $random_backgroud_number; ?>.jpg") no-repeat;
 
 				background-position: center;
 				background-size: cover;
@@ -46,5 +46,16 @@
 		<script async src="resources/libs/javascript/bootstrap.min.js"></script>
 		<script src="resources/libs/javascript/jquery.min.js"></script>
 		<script type="text/javascript" src="scripts/index.min.js"></script>
+
+		<!-- GOOGLE ANALYTICS -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-92815517-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
 	</body>
 </html>
